0c21d57e0c0555f0cd26e39dec9c201e0413e576
Parth Tyagi <parth.27@outlook.com>
2019-12-02 16:01:00 -0600

    Added button callbacks

--- app/src/main/java/com/example/cs125finalproject/MainActivity.java
+++ app/src/main/java/com/example/cs125finalproject/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.cs125finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button pictureBtn = findViewById(R.id.pictureButton);
+        Button importBtn = findViewById(R.id.importButton);
+
+        pictureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                pictureButtonCallback();
+            }
+        });
+
+        importBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                importButtonCallback();
+            }
+        });
+    }
+
+    private void pictureButtonCallback() {
+        Log.println(Log.INFO, "INFO:", "Starting camera app");
+    }
+
+    private void importButtonCallback() {
+        Log.println(Log.INFO, "INFO:", "Starting import");
     }
 }
